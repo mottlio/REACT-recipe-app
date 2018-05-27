82d8ee5576f01e8d7c37f3a4c9ab08332415197a
Michal Mottl <michal.mottl@coleurope.eu>
2018-05-27 22:31:20 +0700

    finish styling navbar

--- src/RecipeApp.js
+++ src/RecipeApp.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import Recipe from './Recipe';
+import Navbar from './Navbar';
 import './RecipeApp.css';
 
 class RecipeApp extends Component {
   render() {
     return (
       <div className="App">
+        <Navbar />
         <Recipe title="pasta" 
         ingredients = {['flour', 'water']}
         instructions="Mix ingredients"
