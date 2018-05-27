e47a4a9a33dad7ce754822cf180feb3212ed8061
Michal Mottl <michal.mottl@coleurope.eu>
2018-05-27 11:21:46 +0700

    add Recipe component with a title prop and display title on index page

---
+++ src/Recipe.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+class Recipe extends Component {
+    render() {
+        return (
+            <div>Recipe {this.props.title}</div>
+        );
+    }
+}
+export default Recipe;
